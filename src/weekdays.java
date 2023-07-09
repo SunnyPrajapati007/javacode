74a2b247d167bb179a8feabd06901fd1dbeb8b0f
sunnyp <007.sunnyprajapati@gmail.com>
2023-07-09 11:55:11 -0400

    java code

---
+++ src/weekdays.java
@@ -0,0 +1,13 @@
+public class weekdays {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 4;
+        int z = 3;
+
+        if (x >= y && x >= z) {
+            System.out.println("x is larger");
+        } else {
+            System.out.println("x is smaller");
+        }
+    }
+}
